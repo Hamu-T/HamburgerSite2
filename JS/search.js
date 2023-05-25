6a3a78253190e99c6aa5d4ef0e0b2c8c95737887
筒井公太郎 <newworld@tsutsuikoutarounoMacBook-Air.local>
2023-05-26 01:58:44 +0900

    add-single

--- JS/search.js
+++ JS/search.js
@@ -7,23 +7,38 @@ function showInputValue() {
       result.innerHTML = input.value;
     });
   }
-
-  /* クリックしたページネーションの色を維持 */
-  document.addEventListener("DOMContentLoaded", function() {
-    let pages = document.querySelectorAll(".c-page--number");
-    
-    for (let i = 0; i < pages.length; i++) {
-      pages[i].addEventListener("click", function() {
-        let currentPage = this;
-        
-        // すべてのページ要素からactiveクラスを削除
-        for (let j = 0; j < pages.length; j++) {
-          pages[j].classList.remove("onColor");
-        }
-        
-        // クリックされたページ要素にactiveクラスを追加
-        currentPage.classList.add("onColor");
-      });
-    }
-  });
   
+// window.onload = function() {
+//     let input = document.querySelector(".inputValue");
+//     let results = document.querySelectorAll(".result");
+    
+//     // クエリパラメータから値を取得し、inputタグのvalueに設定
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const text = urlParams.get('text');
+//     input.value = text;
+    
+//     // resultクラスに値を反映
+//     results.forEach(function(result) {
+//       result.innerHTML = text;
+//     });
+//   };
+
+    /* クリックしたページネーションの色を維持 */
+    document.addEventListener("DOMContentLoaded", function() {
+      let pages = document.querySelectorAll(".c-page--number");
+      
+      for (let i = 0; i < pages.length; i++) {
+        pages[i].addEventListener("click", function() {
+          let currentPage = this;
+          
+          // すべてのページ要素からactiveクラスを削除
+          for (let j = 0; j < pages.length; j++) {
+            pages[j].classList.remove("onColor");
+          }
+          
+          // クリックされたページ要素にactiveクラスを追加
+          currentPage.classList.add("onColor");
+        });
+      }
+    });
+    
